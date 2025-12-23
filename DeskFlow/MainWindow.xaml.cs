ff0fd091a7b782e6d8decb4e555dd4a73a394a51
Valeriy Leskovskiy <valeryleskovsky@yandex.ru>
2025-12-23 07:34:13 +0300

    keyevents

--- DeskFlow/MainWindow.xaml.cs
+++ DeskFlow/MainWindow.xaml.cs
@@ -292,7 +292,7 @@ namespace DeskFlow
         }
 
         // === Задачи ===
-        private void NewTaskTextBox_KeyDown(object sender, KeyEventArgs e)
+        private void NewTaskTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key == Key.Enter && !string.IsNullOrWhiteSpace(NewTaskTextBox.Text))
             {
